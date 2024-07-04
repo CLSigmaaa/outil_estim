8f64c0a329cfc98c8c7a310266e07ac44b0ecd51
Valentin LONGAGNANI <valentin.longagnani@atos.net>
2024-07-04 16:01:26 +0200

    tests

---
+++ __tests__/TreeView.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TreeView from '../components/ui/arborescence/TreeView';
+import { useTreeStore } from '../components/store/useTreeStore';
+
+jest.mock('../components/store/useTreeStore');
+
+describe('TreeView', () => {
+});
