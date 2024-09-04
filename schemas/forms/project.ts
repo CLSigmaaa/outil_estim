3a21f8e7f77019d1b1b9cdf4f8ba88a4933e1a7b
Valentin LONGAGNANI <valentin.longagnani@atos.net>
2024-09-04 10:38:30 +0200

    Initial commit

--- schemas/forms/project.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const createProjectFormSchema = z.object({
-  name: z.string().min(1, { message: "Champ obligatoire." }),
-  description: z.string().min(1, { message: "Champ obligatoire." }),
-})
