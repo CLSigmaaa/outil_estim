8a6b752f2cc8d2c66ab9dd5da42e5a7c7e2d8d4c
Valentin LONGAGNANI <valentin.longagnani@atos.net>
2024-09-16 17:13:02 +0200

    feat: dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,12 @@ import { ModeToggle } from "@/components/dark-mode-toggle";
 export default function TaskPage() {
   return (
     <main className="w-screen h-screen relative flex flex-col justify-center items-center">
-      <DataTable />
+      <div className="relative">
+        <div className="absolute top-[-113px] right-0" >
+          <ModeToggle />
+        </div>
+        <DataTable />
+      </div>
     </main>
   )
 }
